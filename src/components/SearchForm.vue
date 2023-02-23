45c6643a800e4964c00441a9c6ff86efd3581093
ShravyaReddyShankari <33656484+ShravyaReddyShankari@users.noreply.github.com>
2023-02-22 20:22:42 -0600

    Update SearchForm.vue

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -5,22 +5,35 @@
         <div class="submit">
             <button> Find best deal </button>
         </div>
-        <img v-if="bestProductDealData" :src="getProductImageSrc()"/>
-        <p v-if="bestProductDealData">Product Name: {{ bestProductDealData['product_name'] }}</p>
-        <p v-if="bestProductDealData">Product Price: {{ bestProductDealData['product_price'] }}</p>
-        <p v-if="bestProductDealData">Product Rating: {{ bestProductDealData['product_rating'] }}</p>
-        <p v-if="bestProductDealData">Product Link: <a :href="getProductLink()" target="_blank"> View on Website </a></p>
+        <br>
+        <br>
+        <div v-for="(bestProductDealData, index) in bestProductDealDataList">
+            <p v-if="index == 0" class="heading">Best Deal: </p>
+            <div class="row">
+                <div class = "column">
+                    <img v-if="bestProductDealData" :src="getProductImageSrc(index)"/>
+                </div>
+                <div class="column">
+                    <p v-if="bestProductDealData">{{ bestProductDealData['product_name'] }}</p>
+                    <p v-if="bestProductDealData">${{ bestProductDealData['product_price'] }}</p>
+                    <p v-if="bestProductDealData">{{ bestProductDealData['product_rating'] }}</p>
+                    <p v-if="bestProductDealData"><a :href="getProductLink(index)" target="_blank"> View on Website </a></p>
+                </div>
+            </div>
+            <br>
+            <br>
+        </div>
+        
 
     </form>
 </template>
 
 <script>
-import { getBestProductDeal } from '../services/SearchService'
 export default {
  data() {
     return {
         productName: '',
-        bestProductDealData: null,
+        bestProductDealDataList: null
     }
  },
  methods: {
@@ -28,18 +41,18 @@ export default {
         fetch('http://127.0.0.1:8000/best-product-deal/?product_name='+ this.productName).then(async response => {
             if(response.ok){
                 let result = await response.json()
-                this.bestProductDealData = result['best-deal']
-                console.log(this.bestProductDealData)
+                this.bestProductDealDataList = result['best-product-deal-data-list']
+                console.log(this.bestProductDealDataList)
             } else{
                 alert("Server returned " + response.status + " : " + response.statusText);
             }      
         })
     },
-    getProductImageSrc() {
-        return this.bestProductDealData['product_image']
+    getProductImageSrc(index) {
+        return this.bestProductDealDataList[index]['product_image']
     },
-    getProductLink() {
-        return this.bestProductDealData['product_link']
+    getProductLink(index) {
+        return this.bestProductDealDataList[index]['product_link']
     }
  }
 }
@@ -47,7 +60,7 @@ export default {
 
 <style>
     form {
-        max-width: 420px;
+        max-width: 700px;
         margin: 30px auto;
         background: white;
         text-align: left;
@@ -80,7 +93,19 @@ export default {
         color: white;
         border-radius: 20px;
     }
+    img {
+        height: 170px;
+    }
     .submit {
         text-align: center;
+    }
+    .row {
+        display: flex;
+    }
+    .column {
+        flex: 50%;
+    }
+    .heading {
+        font-weight: bold;
     }
 </style>
